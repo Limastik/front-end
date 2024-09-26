57aab4b1c92c0b532d0595a4d491b4ea8d88fc9f
Limastik <Nazariyshved04@gmail.com>
2024-09-26 22:49:00 +0300

    message

---
+++ JavaScript/01/main.js
@@ -0,0 +1,6 @@
+let userAgeValue = 25;
+let userNameNickname = "User123";
+userAgeValue = 30;
+userNameNickname = "NewUserNickname";
+console.log(userAgeValue);        
+console.log(userNameNickname);    
